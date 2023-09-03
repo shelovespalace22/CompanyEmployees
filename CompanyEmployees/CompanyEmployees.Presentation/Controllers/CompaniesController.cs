415cb0dd733295d0a00043642b723838187ca847
shelovespalace22 <miguel.moyano@soy.sena.edu.co>
2023-09-03 12:19:38 -0500

    conf(Backend): Obteniendo una colección de Compañias.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CompanyEmployees.Presentation.ModelBinders;
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects;
@@ -47,14 +48,15 @@ namespace CompanyEmployees.Presentation.Controllers
         }
 
         [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids) 
+        public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids) 
         {
             var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
             
             return Ok(companies); 
         }
 
-        [HttpPost("collection")] public IActionResult CreateCompanyCollection([FromBody] IEnumerable<CompanyForCreationDto> companyCollection) 
+        [HttpPost("collection")] 
+        public IActionResult CreateCompanyCollection([FromBody] IEnumerable<CompanyForCreationDto> companyCollection) 
         {
             var result = _service.CompanyService.CreateCompanyCollection(companyCollection);
             

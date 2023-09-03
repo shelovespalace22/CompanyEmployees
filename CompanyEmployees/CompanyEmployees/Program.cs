feaea9f8fb20136d7e757f8dce78e8300e3697ee
shelovespalace22 <miguel.moyano@soy.sena.edu.co>
2023-09-03 08:29:52 -0500

    conf(Backend): Content Negotiation

--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -14,16 +14,20 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(Program));
 
-
-builder.Services.AddControllers()
-    .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+builder.Services.AddControllers(config => { 
+    config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters()
+    .AddCustomCSVFormatter()
+.AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAutoMapper(typeof(Program));
+
 
 var app = builder.Build();
 
@@ -40,6 +44,7 @@ if (app.Environment.IsProduction())
 //    app.UseHsts();
 
 //Configure the HTTP request pipeline.
+
 //if (app.Environment.IsDevelopment())
 //{
 //    app.UseSwagger();
@@ -49,55 +54,15 @@ if (app.Environment.IsProduction())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles(); 
+
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 { 
     ForwardedHeaders = ForwardedHeaders.All 
 });
+
 app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
-
-//app.Use(async (context, next) =>
-//{
-//    Console.WriteLine($"Logic before executing the next delegate in the Use method");
-
-//    await next.Invoke();
-
-//    Console.WriteLine($"Logic after executing the next delegate in the Use method");
-//});
-
-//app.Map("/usingmapbranch", builder =>
-//{
-//    builder.Use(async (context, next) =>
-//    {
-//        Console.WriteLine("Map branch logic in the Use method before the next delegate");
-//        await next.Invoke();
-//        Console.WriteLine("Map branch logic in the Use meth od after the next delegate");
-//    });
-
-//    builder.Run(async context =>
-//    {
-//        Console.WriteLine($"Map branch response to the client in the Run method");
-//        await context.Response.WriteAsync("Hello from the map branch.");
-//    });
-//});
-
-//app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder =>
-//{
-//    builder.Run(async context =>
-//    {
-//        await context.Response.WriteAsync("Hello from the MapWhen branch.");
-//    });
-//});
-
-//app.Run(async context =>
-//{
-
-//    Console.WriteLine($"Writing the response to the client in the Run method");
-
-//    await context.Response.WriteAsync("Hello from the middleware component.");
-
-//});
 
 app.MapControllers();
 

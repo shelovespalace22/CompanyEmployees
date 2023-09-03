866a0b569874bdb101e7f5f6a22765effa48638e
shelovespalace22 <miguel.moyano@soy.sena.edu.co>
2023-09-03 12:48:04 -0500

    conf(Backend): Eliminando registros Padre e Hijos a la vez.

--- CompanyEmployees/Service.Contracts/ICompanyService.cs
+++ CompanyEmployees/Service.Contracts/ICompanyService.cs
@@ -20,6 +20,6 @@ namespace Service.Contracts
         (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection
             (IEnumerable<CompanyForCreationDto> companyCollection);
 
-
+        void DeleteCompany(Guid companyId, bool trackChanges);
     }
 }

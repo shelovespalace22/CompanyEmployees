1beed6adfebab8cfbb40bfe22b3e90cf81ce1c7b
shelovespalace22 <miguel.moyano@soy.sena.edu.co>
2023-10-20 13:05:24 -0500

    conf (Backend): Capitulo 19 Sorting

--- CompanyEmployees/Repository/EmployeeRepository.cs
+++ CompanyEmployees/Repository/EmployeeRepository.cs
@@ -58,7 +58,8 @@ namespace Repository
             var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
                 .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
                 .Search(employeeParameters.SearchTerm)
-                .OrderBy(e => e.Name)
+                //.OrderBy(e => e.Name)
+                .Sort(employeeParameters.OrderBy)
                 .ToListAsync();
 
             return PagedList<Employee>

3816a031c18f2485a75812429b9d80f851361d61
shelovespalace22 <miguel.moyano@soy.sena.edu.co>
2023-11-02 09:57:13 -0500

    conf(Backend): Capitulo 28 RefreshToken

--- CompanyEmployees/Service.Contracts/IAuthenticationService.cs
+++ CompanyEmployees/Service.Contracts/IAuthenticationService.cs
@@ -14,6 +14,8 @@ namespace Service.Contracts
 
         Task<bool> ValidateUser(UserForAuthenticationDto userForAuth);
 
-        Task<string> CreateToken();
+        Task<TokenDto> CreateToken(bool populateExp);
+
+        Task<TokenDto> RefreshToken(TokenDto tokenDto);
     }
 }
